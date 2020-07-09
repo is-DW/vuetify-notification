9891c3946fe477268d1767f4884e4ec22b53ca71
is-DW <2465970692@qq.com>
2020-07-09 22:31:36 +0800

    initialize project

---
+++ src/components/SnackbarNotify.vue
@@ -0,0 +1,52 @@
+<template>
+  <v-snackbar
+    v-model="snackbar"
+    :bottom="y === 'bottom'"
+    :color="color"
+    :left="x === 'left'"
+    :multi-line="mode === 'multi-line'"
+    :right="x === 'right'"
+    :timeout="timeout"
+    :top="y === 'top'"
+    :vertical="mode === 'vertical'"
+  >
+    {{ text }}
+
+    <template v-slot:action="{ attrs }">
+      <v-btn
+        dark
+        text
+        v-bind="attrs"
+        @click="snackbar = false"
+      >
+        Close
+      </v-btn>
+    </template>
+  </v-snackbar>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator'
+
+enum XDirection {
+  LEFT = 'left',
+  CENTER = 'center',
+  RIGHT = 'right'
+}
+
+enum YDirection {
+  TOP = 'top',
+  BOTTOM = 'bottom'
+}
+
+@Component
+export default class HelloWorld extends Vue {
+  color: string = ''
+  mode: string = ''
+  snackbar: boolean = false,
+  text: string = 'Hello, I\'m a snackbar'
+  timeout: number = 6000
+  x: XDirection = XDirection.RIGHT
+  y: YDirection = YDirection.TOP
+}
+</script>
